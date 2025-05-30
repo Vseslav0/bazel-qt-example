b53f4531e769973504887ad9366952a0801aba03
Revihy <163464928+Vseslav0@users.noreply.github.com>
2025-05-30 17:32:11 +0300

    Add files via upload

---
+++ labs/basics/raycaster/main.cpp
@@ -0,0 +1,15 @@
+#include "Raycaster.h"
+
+#include <QApplication>
+
+//NOLINTBEGIN
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
+
+//NOLINTEND
